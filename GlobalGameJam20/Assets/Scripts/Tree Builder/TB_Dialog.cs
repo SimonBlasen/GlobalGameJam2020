90f9b2dac87a3b2ee1cb3cae5bd5eace585b9805
SimonBlasen <simon.yzf@t-online.de>
2020-01-31 20:33:40 +0100

    Tree Building

--- GlobalGameJam20/Assets/Scripts/Tree Builder/TB_Dialog.cs
+++ GlobalGameJam20/Assets/Scripts/Tree Builder/TB_Dialog.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[ExecuteInEditMode]
+public class TB_Dialog : MonoBehaviour
 {
+    [SerializeField]
+    private TB_Questions from;
+    [SerializeField]
+    private TB_Questions to;
+    [SerializeField]
+    private TextMeshPro textDiagIndex;
+
+    private TB_Questions oldFrom = null;
+    private TB_Questions oldTo = null;
+
+    private Vector3 oldFromPos;
+    private Vector3 oldToPos;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +29,68 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (oldFrom != from)
+        {
+            if (oldFrom != null)
+            {
+                oldFrom.DeRegisterDialog(this);
+            }
+
+            from.RegisterDialog(this);
+
+            oldFrom = from;
+
+            updateSelfRenderPos();
+        }
+        if (oldTo != to)
+        {
+            oldTo = to;
+
+            updateSelfRenderPos();
+        }
+
+        if (from != null)
+        {
+            if (oldFromPos != from.transform.position)
+            {
+                oldFromPos = from.transform.position;
+                updateSelfRenderPos();
+            }
+        }
+        if (to != null)
+        {
+            if (oldToPos != to.transform.position)
+            {
+                oldToPos = to.transform.position;
+                updateSelfRenderPos();
+            }
+        }
+    }
+
+    private void updateSelfRenderPos()
+    {
+        if (to != null && from != null && to != from)
+        {
+            Vector3 posFrom = from.transform.position;
+            Vector3 posTo = to.transform.position;
+
+            transform.position = Vector3.Lerp(posFrom, posTo, 0.5f);
+            transform.up = posFrom - posTo;
+            transform.localScale = new Vector3(0.9f, Vector3.Distance(posFrom, posTo), 0.9f);
+        }
+
+
+    }
+
+    public string DiagTopTag
+    {
+        get
+        {
+            return textDiagIndex.text;
+        }
+        set
+        {
+            textDiagIndex.text = value;
+        }
     }
 }
--- GlobalGameJam20/Assets/Scripts/Tree Builder/TB_Questions.cs
+++ GlobalGameJam20/Assets/Scripts/Tree Builder/TB_Questions.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class TB_Questions : MonoBehaviour
 {
+    [SerializeField]
+    private string[] outDialogues;
+
+    private List<TB_Dialog> outTBDialogues = new List<TB_Dialog>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,32 @@ public class TB_Questions : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void RegisterDialog(TB_Dialog dialog)
+    {
+        if (outTBDialogues.Contains(dialog) == false)
+        {
+            outTBDialogues.Add(dialog);
+        }
+
+        updateOutDiagStringArray();
+    }
+
+    public void DeRegisterDialog(TB_Dialog dialog)
+    {
+        outTBDialogues.Remove(dialog);
+
+        updateOutDiagStringArray();
+    }
+
+    private void updateOutDiagStringArray()
+    {/*
+        outDialogues = new string[outTBDialogues.Count];
+        for (int i = 0; i < outTBDialogues.Count; i++)
+        {
+            outDialogues[i] = outTBDialogues[i].name;
+            outTBDialogues[i].DiagTopTag = i.ToString();
+        }*/
     }
 }
